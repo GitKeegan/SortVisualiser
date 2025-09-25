a859ae97e1df7dfd292f8c5f6d2a9fa880857422
Joshua <xdm510@york.ac.uk>
2025-09-25 16:08:59 +0100

    Created a basic GUI, with a button that outputs "Hi"

--- src/Visualiser.java
+++ src/Visualiser.java
@@ -1,5 +1,32 @@
-public class Visualiser {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Visualiser implements ActionListener {
+    public Visualiser(){
+        JFrame frame = new JFrame("Visualiser");
+        JPanel panel = new JPanel();
+
+        JButton sortButton = new JButton("Sort");
+        sortButton.addActionListener(this);
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 300, 300));
+        panel.setLayout(new GridLayout(0,1));
+        panel.add(sortButton);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Visualiser");
+        frame.pack();
+        frame.setVisible(true);
+    }
     public static void main(String[] args) {
-        System.out.println("Visualiser");
+        new Visualiser();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Hi");
     }
 }
